85e26668699b5d22b6cb95f2acb7d01262dab59e
Allochka11 <allavelychko11@gmail.com>
2022-07-19 16:28:03 +0300

    making style for text

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './Message.css';
+type MessageType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string,
+}
 
-function Message() {
+function Message(props: MessageType) {
     return (
-        <div>
-
+        <div >
+            <div className="message">
+                <img src={props.avatar}></img>
+                <div className={'dialog'}>
+                    <div className={'title'}>{props.name}</div>
+                    <div className={'text'}>{props.message}</div>
+                    <div className={'time'}>{props.time}</div>
+                </div>
+            </div>
         </div>
     )
 }

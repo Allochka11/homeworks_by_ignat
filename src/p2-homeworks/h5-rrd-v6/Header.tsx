c5cccc828a8fa9bd7a397dbc4caa0bb519d03e1d
Allochka11 <allavelychko11@gmail.com>
2022-08-22 16:24:30 +0300

    adding burger menu and navigation

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {NavLink} from "react-router-dom";
+import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import s from './Header.module.css';
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
+    const [openIcon, setIconOpen] = useState<boolean>(false)
+
+    let openIconButton = openIcon ? `${s.menu__body} ${s._active}` : `${s.menu__body}`;
+
+    return (
+        <div className={s.header}>
+            <div className={s.header__container}>
+                <SuperButton className={s.header__logo} onClick={() => setIconOpen(!openIcon)}></SuperButton>
+                <div className={s.header__menu}>
+                    <nav className={openIconButton}>
+                        <ul className={s.menu__list}>
+                            <li><NavLink to={'/pre-junior'}
+                                         className={({isActive}) => isActive ? `${s.active}` : s.menu__link}>Pre-junior </NavLink>
+                            </li>
+                            <li><NavLink to={'/junior'}
+                                         className={({isActive}) => isActive ? s.active : s.menu__link}>Junior </NavLink>
+                            </li>
+                            <li><NavLink to={'/junior_plus'}
+                                         className={({isActive}) => isActive ? s.active : s.menu__link}>Junior+ </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+
+            </div>
+            {/*<NavLink to={'/'}>pre-junior </NavLink>*/}
+            {/*<NavLink to={'/junior'}>junior </NavLink>*/}
+            {/*<NavLink to={'/junior_plus'}>junior+ </NavLink>*/}
         </div>
     )
 }
